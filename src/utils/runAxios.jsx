44679b4e2e767271800208c88199a6dffb7d93ec
nevinmanoj <NEVINMANOJP@GMAIL.COM>
2024-09-06 01:14:42 +0530

    added suffix to api

--- src/utils/runAxios.jsx
+++ src/utils/runAxios.jsx
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const prod = "";
-const devPublic = "https://6vc9m5px-3000.inc1.devtunnels.ms";
 const preProd =
   "https://nnpl-kochi-anguc7hwh6bvhwe2.centralindia-01.azurewebsites.net";
 const dev = "http://localhost:3000";
-export const server = dev;
+export const server = preProd + "/nnpl/api";
 // export const server=prod;
 
 export const runAxios = async (method, data, path, jwtToken, config) => {
